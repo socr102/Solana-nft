627d38ad0e80644c7e1ecae262c51cfda3ab31bc
socr102 <normanburtonfree@gmail.com>
2021-12-18 03:10:08 +0900

    building a connect wallet button with phantom wallet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TEST_GIFS = [
+	'https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp',
+	'https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g',
+	'https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g',
+	'https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp'
+]
 
 const App = () => {
+  //state
+  const[walletAddress, setWalletAddress] = useState(null);
    /*
    * This function holds the logic for deciding if a Phantom Wallet is
    * connected or not
@@ -16,18 +24,14 @@ const App = () => {
       const { solana } = window;
 
       if (solana) {
-        if (solana.isPhantom) {
-          console.log('phantom wallet found!');
           /*
          * The solana object gives us a function that will allow us to connect
          * directly with the user's wallet!
          */
-          const response = await solana.connect({onlyIfTrusted: true});
-          console.log(
-            'Connected with Public key:',
-            response.publicKey.toString()
+          const response = await solana.connect();
+          console.log('Connected with Public key:',response.publicKey.toString()
           );
-        } 
+          setWalletAddress(response.publicKey.toString());
       }else {
           alert('solana object not found! Get a Phantom wallet');
         }
@@ -45,5 +49,47 @@ const App = () => {
    * their wallet to our app yet.
    */
   const renderNotConnectedContainer = () => (
-    <button>
-      Connec
+    <button
+      className = "cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+    /*
+   * When our component first mounts, let's check to see if we have a connected
+   * Phantom Wallet
+   */
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  return (
+    <div className="App">
+      <div className={walletAddress ? 'authed-container': 'container'}>
+        <div className="header-container">
+          <p className="header">🖼 GIF Portal</p>
+          <p className="sub-text">
+            View your GIF collection in the metaverse ✨
+          </p>
+          {!walletAddress && renderNotConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
